85e14a27e68706903562f994e0dbcfeeb931dc45
Syed Ahmed Hussain <syedahmedhussain512@gmail.com>
2025-10-06 23:58:45 -0500

    Signup backend

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://vercel.com/syedahmed-dot-bots-projects/pulsejournal/4sFcv3dZdoJxejcU3f6w16BaTECC"],
+    allow_origins=["https://pulsejournal.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
