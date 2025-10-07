dccbbc281a49e302f14105b7a2ee18301419c3da
Syed Ahmed Hussain <syedahmedhussain512@gmail.com>
2025-10-07 01:42:56 -0500

    passlib updated

--- backend/auth.py
+++ backend/auth.py
@@ -11,7 +11,7 @@ SECRET_KEY = "supersecretkey"      # replace later with env var
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def verify_password(plain, hashed):
